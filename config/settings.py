3a47edf1ff4ed7087fc9e2815ce458751c2ed1ba
Nyangoto <nyangotojosephat62@gmail.com>
2025-12-17 00:56:43 -0800

    Switch to PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -84,12 +84,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# Default to SQLite (for your laptop)
+# Database Configuration
+# If there is a DATABASE_URL in the environment (Render), use it.
+# Otherwise, use the local SQLite file (Your Laptop).
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 # If we are on the Cloud (Render), overwrite it with the real Database
