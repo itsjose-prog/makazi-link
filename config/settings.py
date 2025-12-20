8a1bd3739ea250d9530f2a05942edf719b661787
Nyangoto <nyangotojosephat62@gmail.com>
2025-12-20 08:20:53 -0800

    Fix typo in installed apps

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.humanize'  #standard django apps
+    'django.contrib.humanize',  #standard django apps
 
     # Local Apps (Makazi Link)
     'apps.core',
