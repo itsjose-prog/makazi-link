7933268c48b571be2e2718ec18be9ad9e929e087
Nyangoto <nyangotojosephat62@gmail.com>
2025-12-20 04:50:46 -0800

    Fix missing URL for payment page

--- apps/payments/urls.py
+++ apps/payments/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('pay/<slug:property_slug>/', views.initiate_payment, name='initiate_payment'),
-    # New Path for Safaricom to talk to:
+    # This is the page where they enter their phone number
+    path('initiate/<int:property_id>/', views.initiate_payment, name='initiate_payment'),
+    
+    # This is where Safaricom sends the "Success" message
     path('callback/', views.mpesa_callback, name='mpesa_callback'),
 ]
